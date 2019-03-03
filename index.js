4d1e5cfc82ae7107dae8fc65da68c6fa8512c637
Radosław Madecki <radoslawmadecki@MacBook-Pro-Radosaw.local>
2019-03-03 18:30:36 +0100

    CORS update

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const bodyParser = require('body-parser')
 const port = 9876;
 const noCache = require('nocache')
+const cors = require('cors');
 
 const taskActions = require('./taskActions');
 
 app.use(noCache());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cors())
 
 app.use(taskActions);
 
--- taskActions.js
+++ taskActions.js
@@ -2,27 +2,14 @@ const express = require('express');
 const router = express.Router();
 const dbActions = require('./dbActions');
 const Task = require('./migrations/random_tasks').Task;
-const cors = require('cors');
 
-const whitelist = ['http://localhost:4200', 'http://localhost:9876'];
-
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-router.get('/getTasks', cors(corsOptions), (req, res) => {
+router.get('/getTasks', (req, res) => {
     dbActions.getTasksFromDB().then((tasks) => {
       res.send(tasks)
     })
 })
 
-router.post('/addTask', cors(corsOptions), (req, res) => {
+router.post('/addTask', (req, res) => {
     const taskData = req.body;
     const task = new Task(taskData.name, taskData.desc, taskData.prior, taskData.status);
 
@@ -31,7 +18,7 @@ router.post('/addTask', cors(corsOptions), (req, res) => {
     })
 })
 
-router.put('/editTask', cors(corsOptions), (req, res) => {
+router.put('/editTask', (req, res) => {
     const taskData = req.body;
     const task = new Task(taskData._id, taskData.name, taskData.desc, taskData.prior, taskData.status);
 
